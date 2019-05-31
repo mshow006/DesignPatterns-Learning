d7b223022e1a5812e7f9e9a587072f5743bbdbc2
yingming <1024692198@qq.com>
2019-05-31 15:51:16 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(1 << 30);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(1, 2);
+        map.put(1, 2);
+        map.put(1, 2);
+        map.put(1, 2);
+
+    }
+}
